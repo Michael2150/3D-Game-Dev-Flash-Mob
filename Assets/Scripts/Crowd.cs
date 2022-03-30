1ebec583ee5a29020ac52f84818e16c8d2ca764c
Michael2150 <52236517+Michael2150@users.noreply.github.com>
2022-03-30 14:54:40 +0100

    Crowd Working

---
+++ Assets/Scripts/Crowd.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowd : MonoBehaviour
+{
+    private Animator animator;
+    public float minTime = 2f;
+    public float maxTime = 5f;
+    public float timeForAnim = 5f;
+    public float timeBetween = 10f;
+    private Vector3 startPos;
+    private Quaternion startRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        startPos = transform.position;
+        startRot = transform.rotation;
+
+        StartCoroutine(startAnim());
+    }
+
+    private void Update()
+    {
+        transform.position = startPos;
+    }
+
+    IEnumerator startAnim()
+    {
+        var time = Random.Range(minTime, maxTime);
+        yield return new WaitForSeconds(time);
+
+        animator.SetTrigger("Start");
+        StartCoroutine(waitAnimTime());
+    }
+
+    IEnumerator waitAnimTime()
+    {
+        var time = Random.Range(timeForAnim, timeForAnim + timeBetween);
+        yield return new WaitForSeconds(time);
+
+        
+        transform.rotation = startRot;
+        StartCoroutine(startAnim());
+    }
+}
